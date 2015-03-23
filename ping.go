3130705dd7490c9e1b2356df3eab95081f144aa8
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-03-23 14:21:31 -0700

    changed main functions to the appropriate names

--- ping.go
+++ ping.go
@@ -7,7 +7,7 @@ import (
   )
 
   func main() {
-    db, err := sql.Open("mysql", "root:SOMEPASSWORD@tcp(127.0.0.1:3306)/app")
+    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/app")
   if err != nil {
   panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
   }
