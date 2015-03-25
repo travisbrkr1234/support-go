45350cec92e9bcf462b18a1d952061f61417b529
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-03-25 11:18:13 -0700

    added user input to select queue

--- light_query.go
+++ light_query.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+  "bufio"
+  "os"
   "database/sql"
   _ "github.com/go-sql-driver/mysql"
   "fmt"
   )
 
 		func main() {
-			db, err := sql.Open("mysql", "root:SOMEPASSWORD@tcp(127.0.0.1:3306)/app")
+      consolereader := bufio.NewReader(os.Stdin)
+      fmt.Println("Enter a number")
+      fmt.Println("1 for Phone")
+      fmt.Println("3 for Chat")
+      fmt.Println("5 for Tier2")
+      fmt.Println("7 for Online")
+      input, err := consolereader.ReadString('\n') // this will prompt the user for input
+
+      if err != nil {
+              fmt.Println(err)
+              os.Exit(1)
+      }
+
+			db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/app")
 	  if err != nil {
 	  panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	  }
@@ -17,9 +32,9 @@ import (
 				id int
 				status string
 				)
-				rows, err := db.Query("select id, status from light_status where id = ?",7)
+				rows, err := db.Query("select id, status from light_status where id = ?", input)
 				if err != nil {
-					panic(err)
+					panic("Failed to Login")
 				}
 				defer rows.Close()
 				for rows.Next() {
