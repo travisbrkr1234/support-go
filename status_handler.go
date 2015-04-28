8560354702e4f4c3db71e88addd6e31316698ae4
Brian Boughton <brian@brianboughton.com>
2015-04-28 08:07:08 -0700

    Select all statuses

--- status_handler.go
+++ status_handler.go
@@ -28,7 +28,7 @@ func getStubbedStatuses() Statuses {
 		queue string
 		color string
 	)
-	rows, err := DB.Query("select * from light_status where id = ?", 7)
+	rows, err := DB.Query("select * from light_status")
 	if err != nil {
 		panic("Failed to Login")
 	}
